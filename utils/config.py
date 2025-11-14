0504dc6a07882ed3bee671f5e42201bdac1182fc
박지윤 <awoob@khu.ac.kr>
2025-11-15 00:43:11 +0900

    Remove comment in get_env function
    Remove comment about loading .env file based on main.py.

--- utils/config.py
+++ utils/config.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 
 def get_env(key: str):
-    # ✅ 항상 main.py 기준으로 .env를 불러오도록
     env_path = os.path.join(os.path.dirname(__file__), "..", ".env")
     load_dotenv(dotenv_path=env_path)
     return os.getenv(key)
